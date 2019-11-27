cf0f354d580cb8c5b40f5507ce2c29146b3464e4
lena <anna-lena.raedler@student.uibk.ac.at>
2019-11-27 16:21:54 +0100

    add plugins for task 3

---
+++ exercises/ex05/task3/bar.cpp
@@ -0,0 +1,22 @@
+//
+// Created by lena on 11/27/19.
+//
+
+#include <iostream>
+
+#include "plugin.hpp"
+
+class Bar : public Plugin {
+public:
+    void run() {
+        std::cout << "running Bar plugin" << std::endl;
+    }
+    ~Bar() {
+        std::cout << "deconstructing Bar plugin" << std::endl;
+    }
+
+    extern "C" <std::unique_ptr<Plugin> createPlugin() {
+        std::cout << "creating Bar plugin" << std::endl;
+        return std::make_unique(Bar);
+    }
+};
---
+++ exercises/ex05/task3/foo.cpp
@@ -0,0 +1,22 @@
+//
+// Created by lena on 11/27/19.
+//
+
+#include <iostream>
+
+#include "plugin.hpp"
+
+class Foo : public Plugin {
+public:
+    void run() {
+        std::cout << "running Foo plugin" << std::endl;
+    }
+    ~Bar() {
+        std::cout << "deconstructing Foo plugin" << std::endl;
+    }
+
+    extern "C" <std::unique_ptr<Plugin> createPlugin() {
+        std::cout << "creating Foo plugin" << std::endl;
+        return std::make_unique(Foo);
+    }
+};
