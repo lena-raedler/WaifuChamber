e8a530d53a65aa9174610e9f972ff53f93b3d474
BerndMenia <bernd_menia@yahoo.de>
2019-12-08 14:08:34 +0100

    Stub for ex06 task1

---
+++ exercises/ex06/task1/myVector.cpp
@@ -0,0 +1,9 @@
+//
+// Created by bnorb on 08.12.19.
+//
+
+#include "myVector.hpp"
+
+int main() {
+
+}
---
+++ exercises/ex06/task1/myVector.hpp
@@ -0,0 +1,15 @@
+//
+// Created by bnorb on 08.12.19.
+//
+
+#pragma once
+
+
+class myVector {
+public:
+    myVector();
+    ~myVector();
+
+private:
+
+};
