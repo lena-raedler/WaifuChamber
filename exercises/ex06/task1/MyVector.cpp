d0c39d361466150af2829c5eb6e2da879818a0eb
BerndMenia <bernd_menia@yahoo.de>
2019-12-09 16:09:11 +0100

    Copy- constructor and assignment for MyVector

--- exercises/ex06/task1/MyVector.cpp
+++ exercises/ex06/task1/MyVector.cpp
@@ -3,20 +3,41 @@
 //
 
 //#include "MyVector.hpp"
+#include <cstring>
 
 template <typename T, int Length>
 class MyVector {
     public:
         MyVector() {
-            //arr[Length] = {};
             arr = new T[Length];
+            size = Length;
+        }
+
+        MyVector(const MyVector& from) {
+            size = from.size;
+            arr = new T[size];
+            memcpy(arr, from.arr, size);
         }
 
         ~MyVector() {
             delete[] arr;
         }
 
+        MyVector& operator =(const MyVector& from) {
+            if (&from == this) {
+                return *this;
+            }
+
+            // What if arr has a different size?
+            arr = from.arr;
+            size = from.size;
+            return *this;
+
+            //arr = new T[from.size];
+            //memcpy(arr, from.data, from.size);
+        }
+
     private:
-        //T arr[];
         T* arr;
+        int size;
 };
--- exercises/ex06/task1/main.cpp
+++ exercises/ex06/task1/main.cpp
@@ -6,9 +6,24 @@
 #include "MyVector.cpp"
 
 int main () {
-    std::cout << "Creating an int vector" << std::endl;
-    MyVector<int, 5> vec();
+    {
+        std::cout << "Creating an int vector" << std::endl;
+        MyVector<int, 5> vecInt;
+    }
 
-    std::cout << "Creating a double vector" << std::endl;
-    MyVector<double, 5> vec2();
+    {
+        std::cout << "Creating a double vector" << std::endl;
+        MyVector<double, 5> vecDouble;
+    }
+
+    {
+        std::cout << "Creating a float vector" << std::endl;
+        MyVector<float, 5> vecFloat;
+    }
+
+    {
+        std::cout << "Testing copy- constructor and assignment" << std::endl;
+        MyVector<int, 5> vec1;
+        MyVector<int, 5> vec2(vec1);
+    }
 }
