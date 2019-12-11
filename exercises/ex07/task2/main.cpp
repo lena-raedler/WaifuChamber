52e350b729eb5d28256d8c15fa65ca9263a9904e
lena <anna-lena.raedler@student.uibk.ac.at>
2019-12-11 13:19:19 +0100

    move and copy work as well -> only tested as its included and i am lazy

--- exercises/ex07/task2/main.cpp
+++ exercises/ex07/task2/main.cpp
@@ -12,5 +12,11 @@ int main() {
 
     Vector<4> vec1{1.0, 2.0, 3.0, 4.0};
     std::cout << vec1.to_string() << std::endl;
-    return 0;
+
+    Vector<4> vec2 = vec1;
+    std::cout << vec2.to_string() << std::endl;
+
+    Vector<1> vec3(std::move(vec));
+    std::cout << vec3.to_string() << std::endl;
+
 }
