4240df6ee6504f6d64d3ce75e05dd69c157b9593
BerndMenia <bernd_menia@yahoo.de>
2019-12-28 18:50:32 +0100

    Add pretty print for vector.

--- exercises/ex07/task1/Person.hpp
+++ exercises/ex07/task1/Person.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
 struct Person {
@@ -7,3 +8,11 @@ struct Person {
     std::string last_name;
     int age;
 };
+
+std::string to_string(const Person &person) {
+    return "Name: " + person.first_name + ' ' + person.last_name + ", Age: " + std::to_string(person.age);
+}
+
+std::ostream& operator<<(std::ostream& os, const Person& person) {
+    return os << to_string(person) << std::endl;
+}
--- exercises/ex07/task1/main.cpp
+++ exercises/ex07/task1/main.cpp
@@ -1,22 +1,27 @@
-
 #include "Person.hpp"
 #include <iostream>
 #include <memory>
 #include <vector>
 
-std::vector<std::shared_ptr<Person>> createVector();
+std::vector<std::shared_ptr<Person>> createAndFillVector();
 std::vector<Person*> mapPerson(const std::vector<std::shared_ptr<Person>>& input);
+void printVector(const std::vector<std::shared_ptr<Person>>& vectorToPrint);
 
 
 int main() {
-    createVector();
+    std::cout << "Create vector..." << std::endl;
+    auto vector = createAndFillVector();
+    std::cout << "Done!" << std::endl;
+
+    printVector(vector);
 }
 
+std::vector<std::shared_ptr<Person>> createAndFillVector() {
+    auto person1 = std::make_shared<Person>(Person{"FirstName1", "LastName1", 1});
+    auto person2 = std::make_shared<Person>(Person{"FirstName2", "LastName2", 2});
+    auto person3 = std::make_shared<Person>(Person{"FirstName3", "LastName3", 3});
 
-std::vector<std::shared_ptr<Person>> createVector() {
-    std::shared_ptr<Person> person1 = std::make_shared<Person>(new Person {"FirstName2", "LastName2", 2});
-
-    std::vector<std::shared_ptr<Person>> vector
+    std::vector<std::shared_ptr<Person>> vector {person1, person1, person2, person3};
     return vector;
 }
 
@@ -25,4 +30,11 @@ std::vector<Person*> mapPerson(const std::vector<std::shared_ptr<Person>>& input
     // dummy
     std::vector<Person*> output;
     return output;
+}
+
+void printVector(const std::vector<std::shared_ptr<Person>>& vectorToPrint) {
+    std::cout << "Print vector: " << std::endl;
+    for (const auto& person_ptr : vectorToPrint) {
+        std::cout << *person_ptr << std::endl;
+    }
 }
