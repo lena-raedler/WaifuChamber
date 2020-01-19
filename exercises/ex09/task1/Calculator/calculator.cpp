78d6668ea0e486800a5b1644a5963ff879284849
BerndMenia <bernd_menia@yahoo.de>
2020-01-19 20:16:13 +0100

    Lol I broke it

--- exercises/ex09/task1/Calculator/calculator.cpp
+++ exercises/ex09/task1/Calculator/calculator.cpp
@@ -5,6 +5,11 @@ Calculator::Calculator(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Calculator)
     , input("")
+    , inputNumbersCount(0)
+    , inputOperationsCount(0)
+    , inputNumbers(16)
+    , inputOperations(16)
+
 {
     ui->setupUi(this);
 }
@@ -15,25 +20,93 @@ Calculator::~Calculator()
 }
 
 
-void Calculator::on_pushButton_clicked()
+void Calculator::on_pushButton_0_clicked()
 {
-    input += "0";
+    //input += "0";
+    inputNumbers[inputNumbersCount].push_back('0');
+    updateInput();
+}
+
+void Calculator::on_pushButton_1_clicked()
+{
+    //input += "1";
+    inputNumbers[inputNumbersCount].push_back('1');
     updateInput();
 }
 
 void Calculator::on_pushButton_2_clicked()
 {
-    input += "1";
+    input += "2";
     updateInput();
 }
 
 void Calculator::on_pushButton_3_clicked()
 {
-    input += "2";
+    input += "3";
+    updateInput();
+}
+
+void Calculator::on_pushButton_4_clicked()
+{
+    input += "4";
+    updateInput();
+}
+
+void Calculator::on_pushButton_5_clicked()
+{
+    input += "5";
+    updateInput();
+}
+
+void Calculator::on_pushButton_6_clicked()
+{
+    input += "6";
+    updateInput();
+}
+
+void Calculator::on_pushButton_7_clicked()
+{
+    input += "7";
+    updateInput();
+}
+
+void Calculator::on_pushButton_8_clicked()
+{
+    input += "8";
+    updateInput();
+}
+
+void Calculator::on_pushButton_9_clicked()
+{
+    input += "9";
+    updateInput();
+}
+
+void Calculator::on_pushButton_plus_clicked()
+{
+    //input += "+";
+    //inputNumbers.push_back("+");
+    inputOperations.push_back("+");
+    inputOperationsCount++;
     updateInput();
 }
 
 void Calculator::updateInput()
 {
-     ui->label->setText(input);
+     //ui->label_input->setText(input);
+     QString inputS = "";
+     //int max = (inputNumbersCount >= inputOperationsCount) ? inputNumbersCount : inputOperationsCount;
+     for (int i = 0; i <= inputNumbersCount; i++) {
+         inputS += inputNumbers[i];
+         inputS += inputOperations[i];
+     }
+     ui->label_input->setText(inputS);
+}
+
+
+void Calculator::on_pushButton_equals_clicked()
+{
+    //int max = if (inputArr)
+
+
 }
--- exercises/ex09/task1/Calculator/calculator.h
+++ exercises/ex09/task1/Calculator/calculator.h
@@ -16,13 +16,28 @@ public:
     ~Calculator();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_0_clicked();
+    void on_pushButton_1_clicked();
     void on_pushButton_2_clicked();
     void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_plus_clicked();
+
+    void on_pushButton_equals_clicked();
 
 private:
     Ui::Calculator *ui;
     QString input;
+    int inputNumbersCount;
+    int inputOperationsCount;
+    QVector<QString> inputNumbers;
+    QVector<QString> inputOperations;
     void updateInput();
 };
 #endif // CALCULATOR_H
