43bcc135b4acf7fa7348ff53d8680a78547dedac
Lukas <lukas.aukenthaler@student.uibk.ac.at>
2020-01-21 14:59:02 +0100

    M O T I V A T I O N

---
+++ exercises/ex09/task2/config.cpp
@@ -0,0 +1,36 @@
+//
+// Created by auki on 21.01.20.
+//
+#include <cstring>
+#include <iostream>
+#include <string>
+
+struct Config{
+#define CONFIG(_type, _identifier, _default) _type _identifier = _default;
+#include "config.def"
+};
+
+void serialize(Config& c,unsigned char* buffer){
+#define CONFIG(_type, _identifier, _default) \
+std::memcpy(buffer, &(c._identifier), sizeof(c._identifier)); \
+buffer += sizeof(c._identifier);
+#include "config.def"
+}
+
+void deSerialize(Config& c,unsigned char* buffer){
+#define CONFIG(_type, _identifier, _default) \
+std::memcpy(&(c._identifier), buffer, sizeof(c._identifier)); \
+buffer += sizeof(c._identifier);
+#include "config.def"
+}
+
+
+int main() {
+    Config con;
+    Config con2;
+    con.height = 666;
+    unsigned char buf[258];
+    serialize(con, buf);
+    deSerialize(con2, buf);
+    std::cout << con2.height << std::endl;
+}
