f1be1309726e0dcdcb5c840b17db6534e074f543
Lukas <lukas.aukenthaler@student.uibk.ac.at>
2020-01-28 14:07:15 +0100

    fixed some weird shit

--- sideproject/src/game/entities/player/EnemyBuilder.cpp
+++ sideproject/src/game/entities/player/EnemyBuilder.cpp
@@ -11,6 +11,7 @@ namespace EnemyBuilder {
         switch(id){
             case 1: //Skelly
                 e.position = utility::convert(pos);
+                e.velocity = {0,0};
                 e.setMaxHealth(10);
                 e.speed = 10;
                 e.ai = PATROL;
@@ -39,6 +40,36 @@ namespace EnemyBuilder {
                 }
                 GlobalObjects::enemies.push_back(e);
                 break;
+            case 2://floating skelly
+                e.position = utility::convert(pos);
+                e.velocity = {0,0};
+                e.setMaxHealth(10);
+                e.speed = 10;
+                e.ai = PATROL;
+                e.gravityType = NOGRAVITY;
+                utility::fillDefaultHitbox(e.hitbox);
+
+                utility::fillDefaultHitbox(p.hitbox);
+                p.damage = 20;
+                p.timeToLive = 10000;
+                p.usesPlatforms = false;
+                p.fragile = true;
+                p.gravityType = NOGRAVITY;
+                p.baseInit();
+
+                a.projectiles.push_back(p);
+                a.speed = 20;
+                a.cooldown = 1000;
+
+                e.abilities.push_back(a);
+                e.textureLocation = "files/textures/skeleton_01.png";
+
+                //rectangle is filled with the given position of the enemy and the global tilesize
+                {
+                    SDL_Rect r = {pos.first, pos.second, GlobalConstants::tileSize, GlobalConstants::tileSize};
+                    e.rec = std::make_shared<SDL_Rect>(r);
+                }
+                GlobalObjects::enemies.push_back(e);
             default:
                 break;
         }
