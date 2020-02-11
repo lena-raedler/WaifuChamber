83fa4ae0fcd6158f5d733fe40364748dfb0393e3
lena <anna-lena.raedler@student.uibk.ac.at>
2020-02-11 17:27:49 +0100

    added checkpoints, enemies, and bosses to third area

--- sideproject/src/game/game.cpp
+++ sideproject/src/game/game.cpp
@@ -214,6 +214,16 @@ void Game::makeCheckpoints(){
         c.rectangle = {(int)c.position.x, (int)c.position.y, GlobalConstants::tileSize, GlobalConstants::tileSize};
         GlobalObjects::checkpoints.push_back(c);
     }
+    {
+        Checkpoint c;
+        c.position = utility::convert({42, 13});
+        utility::fillDefaultHitbox(c.hitbox);
+        c.room = "files/rooms/desert1.txt";
+        c.id = GlobalObjects::checkpoints.size();
+        c.texture = renderer->createTextureFromSurface(s);
+        c.rectangle = {(int)c.position.x, (int)c.position.y, GlobalConstants::tileSize, GlobalConstants::tileSize};
+        GlobalObjects::checkpoints.push_back(c);
+    }
     SDL_FreeSurface(s);
 
     player.lastCP = &(GlobalObjects::checkpoints[0]);
